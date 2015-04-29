5032364dd2ba6d8f8526a6a92b127207976ff40b
Maurits van Rees <maurits@vanrees.org>
2015-04-29 23:18:17 +0200

    Back to development: 1.0.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.3'
+version = '1.0.4.dev0'
 desc = "Infrastructure for maintaining a registry of available behaviors"
 doc_files = [
     "README.rst",
