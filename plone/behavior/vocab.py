f0a91cadc2ed4a2c4fde8d65c4461c2fd3cd8f33
Jens W. Klein <jk@kleinundpartner.at>
2015-05-06 16:58:28 +0200

    use decorator

--- plone/behavior/vocab.py
+++ plone/behavior/vocab.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from plone.behavior.interfaces import IBehavior
 from zope.component import getUtilitiesFor
-from zope.interface import directlyProvides
+from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 
 
+@provider(IVocabularyFactory)
 def BehaviorsVocabularyFactory(context):
     behaviors = getUtilitiesFor(IBehavior)
-    items = [(reg.title, reg.interface.__identifier__)
-             for (title, reg) in behaviors]
+    items = [
+        (reg.title, reg.interface.__identifier__) for (title, reg) in behaviors
+    ]
     return SimpleVocabulary.fromItems(items)
-
-directlyProvides(BehaviorsVocabularyFactory, IVocabularyFactory)
