a98dbfee3508920678fc6befe3a9b9c0dee9fef6
David Glick <david@glicksoftware.com>
2016-10-23 12:24:30 -0400

    declare support

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
     ],
