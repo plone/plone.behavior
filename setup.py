195022b143b87e87be5070850ad6b158c3c7207f
Maurits van Rees <maurits@vanrees.org>
2018-01-17 12:00:38 +0100

    Added 'Framework :: Plone :: 5.2' to classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
