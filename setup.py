0063cb22f0654884413352113ed9461a884626a7
Maurits van Rees <maurits@vanrees.org>
2020-04-17 01:42:07 +0200

    Unify Python versions in setup.py, tox and Travis.
    Added 3.5 to setup.py classifiers.
    Removed testing for 3.4 from Travis, added 3.7 and 3.8.
    Removed testing for 3.3 and 3.4 from tox, added 3.6, 3.7 and 3.8.
    
    No code change, so no news snippet.
    And no Jenkins jobs needed.

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Framework :: Plone :: Core",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
