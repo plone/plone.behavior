575b6bca185a71259391259d1cdbdeec97d5ee6e
Maurits van Rees <maurits@vanrees.org>
2020-08-27 13:58:18 +0200

    Bumped version for feature release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.3.3.dev0'
+version = '1.4.0.dev0'
 desc = "Infrastructure for maintaining a registry of available behaviors"
 doc_files = [
     "README.rst",
