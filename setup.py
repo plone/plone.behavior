8c89e4b399c48997e88e1362ad9e2b779c4e386c
Jens W. Klein <jk@kleinundpartner.at>
2023-04-12 14:09:55 +0200

    fix metadata and bump version
    (cherry picked from commit 3c8040405334367e21e7c3d206a6b35c4943f10e)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "1.4.1.dev0"
+version = "2.0.0.dev0"
 desc = "Infrastructure for maintaining a registry of available behaviors"
 doc_files = [
     "README.rst",
@@ -20,27 +20,28 @@ setup(
     classifiers=[
         "Development Status :: 6 - Mature",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.1",
-        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
     ],
     keywords="Plone behavior registry",
     author="Martin Aspeli",
     author_email="optilude@gmail.com",
-    url="https://pypi.org/project/plone.behavior",
+    maintainer="Plone Foundation - Release Team",
+    maintainer_email="releaseteam@plone.org",
+    url="https://github.com/plone/plone.behavior",
     license="BSD",
     packages=find_packages(),
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
         "setuptools",
         "zope.annotation",
