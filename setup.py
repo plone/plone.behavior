2911cbfc31facb1ae8ae8e7154fb986db67bc246
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-04-15 10:59:37 +0200

    Back to development: 2.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "2.0.0"
+version = "2.0.1.dev0"
 desc = "Infrastructure for maintaining a registry of available behaviors"
 doc_files = [
     "README.rst",
