bc230fe1f88e1d1848eca8cf9e4c564ab502c0c3
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-11-08 11:38:28 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "2.0.4.dev0"
+version = "3.0.0.dev0"
 desc = "Infrastructure for maintaining a registry of available behaviors"
 
 long_description = (
