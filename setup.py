025496550f4a95eb9f8bdecd2884f1c695d451ee
Maurits van Rees <maurits@vanrees.org>
2025-11-19 21:11:39 +0100

    Bumped version for breaking release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import setup
 
 
-version = "3.0.0.dev0"
+version = "3.0.0a1.dev0"
 desc = "Infrastructure for maintaining a registry of available behaviors"
 
 long_description = (
